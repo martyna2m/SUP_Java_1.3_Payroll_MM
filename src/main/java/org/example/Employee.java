9be16769b217277b2f2bb0686245ec6eec05df1e
martyna2m <bsom2m@gmail.com>
2023-10-11 14:13:38 +0200

    Merge employee methods

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Employee extends Company {
@@ -8,18 +9,25 @@ public class Employee extends Company {
     private String lastName;
     private double salary;
 
-
-    public Employee(String firstName, String lastName, double salary) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.salary = salary;
+    public Employee() {
         allEmployees.add(this);
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 
     public double getSalary() {
         return salary;
     }
-
 
     @Override
     public String toString() {
@@ -30,36 +38,57 @@ public class Employee extends Company {
                 ;
     }
 
-    public static void addEmployees() {
-        Program.printInitialQuestion();
-        Scanner scan = new Scanner(System.in);
+    public static void addEmployees() throws InputMismatchException {
+        try {
+            Program.printInitialQuestion();
+            Scanner scan = new Scanner(System.in);
+            int numberOfEmployees = scan.nextInt();
 
-        int numberOfEmployees = scan.nextInt();
-        for (int i = 0; i < numberOfEmployees; i++) {
-            Program.printInstruction(i + 1);
-
-            String firstName = scan.next();
-            String lastName = scan.next();
-            double salary = scan.nextDouble();
-            new Employee(firstName, lastName, salary);
-
-            Program.printSuccessInfo(i + 1);
+            for (int i = 0; i < numberOfEmployees; i++) {
+                Program.printEmployeeIndex(i + 1);
+                addSingleEmployee();
+                Program.printSuccessInfo(i + 1);
+            }
+        } catch (Exception e) {
+            System.out.println("You can only choose an integer as an option. Please start again or use options from the menu.");
         }
-
     }
 
-    public static void addSingleEmployee() {
+
+    public static void addSingleEmployee() throws InputMismatchException {
         Scanner scan = new Scanner(System.in);
-        Program.printSingleInstruction();
+        Employee employee = new Employee();
 
-        String firstName = scan.next();
-        String lastName = scan.next();
+        Program.printNameInstruction();
+        String firstName = scan.nextLine();
+        while (firstName.isEmpty()) {
+            System.out.println("The value can't be null");
+            firstName = scan.nextLine();
+        }
+        employee.setFirstName(firstName);
+
+        Program.printLastNameInstruction();
+        String lastName = scan.nextLine();
+        while (lastName.isEmpty()) {
+            System.out.println("The value can't be null");
+            lastName = scan.nextLine();
+        }
+        employee.setLastName(lastName);
+
+        Program.printSalaryInstruction();
         double salary = scan.nextDouble();
-        new Employee(firstName, lastName, salary);
+        while (salary < 0) {
+            System.out.println("Salary must be grater than 0.");
+            salary = scan.nextDouble();
+        }
+        employee.setSalary(salary);
 
-        Program.printSingleSuccessInfo();
     }
 
 
 }
+
+
+
+
 
